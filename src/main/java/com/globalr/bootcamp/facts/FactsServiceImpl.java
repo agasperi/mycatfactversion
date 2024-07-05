402e261d96a21cd24758897986ad1d7b2ea8b9a1
agasperi <agasperi@gmail.com>
2024-07-04 19:37:38 -0500

    Comentando la clase FactsServiceImpl
    Esto es una prueba

--- src/main/java/com/globalr/bootcamp/facts/FactsServiceImpl.java
+++ src/main/java/com/globalr/bootcamp/facts/FactsServiceImpl.java
@@ -4,6 +4,8 @@ import com.globalr.bootcamp.client.rest.model.CatFacts;
 import com.globalr.bootcamp.facts.FactsService;
 import org.springframework.stereotype.Service;
 
+/** FactsServiceImpl.                 */
+/*  Un comentario para probar el pull */
 @Service
 public class FactsServiceImpl implements FactsService {
     @Override
